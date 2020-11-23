d9e3ffa2c09d23626f3ed8e0443531d2ae9fa28e
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2020-11-23 23:55:56 +0200

    Create Main an detail screens Till Lesson 188

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final double price;
+
+  ProductItem(
+    this.id,
+    this.title,
+    this.imageUrl,
+    this.price,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        header: Container(
+          color: Colors.black54,
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            '$price',
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
