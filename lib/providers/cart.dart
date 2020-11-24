d13bc6a4e62f095c88f8db92b1c13732edf75ce9
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2020-11-24 23:53:04 +0200

    using snackbar for add to cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -58,6 +58,26 @@ class Cart with ChangeNotifier {
     _items.clear();
     notifyListeners();
   }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCardItem) => CartItem(
+          id: existingCardItem.id,
+          title: existingCardItem.title,
+          quantity: existingCardItem.quantity - 1,
+          price: existingCardItem.price,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
 }
 
 class CartItem {
