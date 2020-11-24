e8be235d4b8a8f2285d6ed46873178df6eed00cd
MedhatMohamed5 <medhat0103932@sci.asu.edu.eg>
2020-11-24 12:36:11 +0200

    Add favorite state, make products overview screen as Stateful this is better than using provider (comments) and make it a local state not public state Lesson 198

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+// import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  bool _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
+    // final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'MyShop - Home',
         ),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  // productsContainer.showFavoritesOnly();
+                  _showOnlyFavorites = true;
+                } else if (selectedValue == FilterOptions.All) {
+                  // productsContainer.showAll();
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text(
+                  'Only Favorites',
+                ),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text(
+                  'Show all',
+                ),
+                value: FilterOptions.All,
+              ),
+            ],
+            icon: Icon(
+              Icons.more_vert,
+            ),
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
